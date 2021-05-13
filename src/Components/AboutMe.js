f6283f73825ae157dde05d4e756ba78a3ec84a7c
sonyadawar <44790119+sanchitdawar@users.noreply.github.com>
2021-05-13 22:02:45 +0530

    update

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -3,8 +3,15 @@ import React from 'react'
 const AboutMe=()=>{
 
 
-    return(
+    return (
+          <div>
+            <h1>About me.</h1>
+             <p> Hi I am Akshay Saini, a 25 year old Web developer, YouTuber, and a Digital Marketer living in Hyderabad, India. I am a Computer Science Engineer, currently working with awesome folks at Uber.
 
+Have a look at my YouTube Channel, skills or just connect with me on LinkedIn. I am always excited to do business with like minded people, lets discuss over coffee.
+</p>
+
+          </div>
     )
     
 }
